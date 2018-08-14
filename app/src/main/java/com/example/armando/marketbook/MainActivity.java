f98b9cfa90df6dce7dd6890f2c5e0386e7f35453
ArmandoOspedale <38313476+ArmandoOspedale@users.noreply.github.com>
2018-08-15 01:42:37 +0200

    Implementata la ricerca

--- app/src/main/java/com/example/armando/marketbook/MainActivity.java
+++ app/src/main/java/com/example/armando/marketbook/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.armando.marketbook;
 
+import android.app.Activity;
 import android.app.ActivityOptions;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
@@ -18,23 +23,28 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.view.animation.AnimationUtils;
 import android.widget.EditText;
-import android.widget.ListView;
+import android.widget.ImageView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -50,11 +60,13 @@ public class MainActivity extends AppCompatActivity {
     //Dati
     private ArrayList<ArrayList<Book>> libriCategoria;
     private ArrayList<String> nomeCategoria;
+    private  DocumentSnapshot documentSnapshot;
 
     //View
     private Toolbar toolbar;
     private RecyclerView mRecyclerView;
     private  BottomNavigationView bottomNavigationView;
+    private ImageView sfondoSfocato;
 
     //Database
     private FirebaseFirestore db;
@@ -76,28 +88,63 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.search_menu, menu);
         MenuItem mSearch = menu.findItem(R.id.m_search);
-        SearchView mSearchView = (SearchView) mSearch.getActionView();
+        final SearchView mSearchView = (SearchView) mSearch.getActionView();
         mSearchView.findViewById(android.support.v7.appcompat.R.id.search_plate).setBackground(null);
         EditText et= mSearchView.findViewById(android.support.v7.appcompat.R.id.search_src_text);
         et.setHintTextColor(getResources().getColor(R.color.sfondomedio));
         et.setHint("Cerca libri, autori...");
         mSearchView.setIconified(false);
         mSearchView.clearFocus();
-        final ListView listView = findViewById(R.id.list_view);
+        final RecyclerView risultati = findViewById(R.id.risultati);
+        sfondoSfocato = findViewById(R.id.sfocato);
+        LinearLayoutManager layoutManager=new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false);
+        risultati.setLayoutManager(layoutManager);
+        risultati.setHasFixedSize(true);
+        risultati.setLayoutManager(layoutManager);
         mSearch.setOnActionExpandListener(new MenuItem.OnActionExpandListener() {
             @Override
             public boolean onMenuItemActionExpand(MenuItem item) {
-                listView.setVisibility(View.VISIBLE);
+                Bitmap map=takeScreenShot(MainActivity.this);
+                Bitmap fast=fastblur(map);
+                Drawable draw=new BitmapDrawable(getResources(),fast);
+                sfondoSfocato.setImageDrawable(draw);
+                risultati.setVisibility(View.VISIBLE);
+                sfondoSfocato.setVisibility(View.VISIBLE);
+                db.collection("Index").document("sZYBGqY7z7gXeFEjodCW").get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        DocumentSnapshot documentSnapshot = task.getResult();
+                        Map<String,Object> x =documentSnapshot.getData();
+                        List<String> indiceLibri = (List<String>) x.get("libri");
+                        List<String> indiceAutori = (List<String>) x.get("autori");
+                        ricerca(mSearchView,risultati,indiceLibri,indiceAutori);
+                    }
+                });
                 return true;
             }
 
             @Override
             public boolean onMenuItemActionCollapse(MenuItem item) {
-                listView.setVisibility(View.GONE);
+                risultati.setVisibility(View.GONE);
+                new RisultatiAdapter(getApplicationContext()).destroy();
+                sfondoSfocato.setVisibility(View.GONE);
                 return true;
             }
         });
+        return super.onCreateOptionsMenu(menu);
+    }
 
+    private List<String> buildArray(List<String> indice, String stringa) {
+        List<String> riscontri = new ArrayList<>();
+        for (int i=0;i<indice.size();i++){
+            if(indice.get(i).toLowerCase().contains(stringa.toLowerCase())){
+                riscontri.add(indice.get(i));
+            }
+        }
+        return riscontri;
+    }
+
+    private void ricerca(SearchView mSearchView, final RecyclerView risultati, final List<String> indiceLibri, final List<String> indiceAutori) {
         mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
@@ -105,14 +152,71 @@ public class MainActivity extends AppCompatActivity {
             }
 
             @Override
-            public boolean onQueryTextChange(String newText) {
+            public boolean onQueryTextChange(final String newText) {
+                final List<Object> risultatiQuery = new ArrayList<>();
+                if(!newText.equals("")){
+                    List<String> riscontriLibri = buildArray(indiceLibri,newText);
+                    final List<String> riscontriAutori = buildArray(indiceAutori,newText);
+                    ArrayList<Task> taskArrayList = new ArrayList<>();
+                    if(riscontriAutori.size()>5){
+                        for (int i=0;i<5;i++){ //prendo i primi 5 risultati
+                            taskArrayList.add(db.collection("Autori").whereEqualTo("Nome",riscontriAutori.get(i)).get()) ;
+                        }
+                    }else{
+                        for (int i=0;i<riscontriAutori.size();i++){ //prendo i primi 5 risultati
+                            taskArrayList.add(db.collection("Autori").whereEqualTo("Nome",riscontriAutori.get(i)).get()) ;
+                        }
+                    }
+                    if(riscontriLibri.size()>5){
+                        for (int i=0;i<5;i++){ //prendo i primi 5 risultati
+                            taskArrayList.add(db.collection("Libri").whereEqualTo("Titolo",riscontriLibri.get(i)).get());
+                        }
+                    }else{
+                        for (int i=0;i<riscontriLibri.size();i++){ //prendo i primi 5 risultati
+                            taskArrayList.add(db.collection("Libri").whereEqualTo("Titolo",riscontriLibri.get(i)).get());
+                        }
+                    }
+                    Tasks.whenAllSuccess(taskArrayList.toArray(new Task[taskArrayList.size()])).addOnSuccessListener(new OnSuccessListener<List<Object>>() {
+                        @Override
+                        public void onSuccess(List<Object> list) {
+                            List<List<DocumentSnapshot>> Documenti= new ArrayList<>();
+                            for(int i=0;i<list.size();i++){
+                                QuerySnapshot QueryLibri = (QuerySnapshot) list.get(i);
+                                Documenti.add(QueryLibri.getDocuments());
+                            }
+                            for(int y=0; y< Documenti.size(); y++){
+                                Book book = Documenti.get(y).get(0).toObject(Book.class);
+                                if(book.getTitolo()==null){
+                                    Autore autore = Documenti.get(y).get(0).toObject(Autore.class);
+                                    risultatiQuery.add(autore);
+                                }
+                                else {risultatiQuery.add(book);}
+                            }
+
+                            for(int i=0;i<risultatiQuery.size();i++){
+                                try {
+                                    Book book = (Book) risultatiQuery.get(i);
+                                    Log.d("Risultato", book.getTitolo());
+                                }catch (Exception e){
+                                    Autore autore = (Autore) risultatiQuery.get(i);
+                                    Log.d("Risultato", autore.getNome());
+                                }
+                            }
+
+                            RisultatiAdapter adapter = new RisultatiAdapter(getApplicationContext(),risultatiQuery);
+                            risultati.setAdapter(adapter);
+                        }
+                    });
+                } else {
+                    risultatiQuery.clear();
+                    RisultatiAdapter adapter = new RisultatiAdapter(getApplicationContext(),risultatiQuery);
+                    risultati.setAdapter(adapter);
+                }
 
                 return false;
             }
         });
-        return super.onCreateOptionsMenu(menu);
     }
-
 
     private void setupFirebase() {
         mAuth = FirebaseAuth.getInstance();
@@ -220,9 +324,7 @@ public class MainActivity extends AppCompatActivity {
             }
 
             @Override
-            public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-
-            }
+            public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) { }
 
             @Override
             public int getItemCount() {
@@ -238,6 +340,187 @@ public class MainActivity extends AppCompatActivity {
 
     private void setupWindowAnimations() {
         getWindow().setExitTransition(TransitionInflater.from(this).inflateTransition(R.transition.activity_slide));
+    }
+
+    private Bitmap takeScreenShot(Activity activity) {
+        View view = activity.getWindow().getDecorView();
+        view.setDrawingCacheEnabled(true);
+        view.buildDrawingCache();
+        Bitmap b1 = view.getDrawingCache();
+        Rect frame = new Rect();
+        activity.getWindow().getDecorView().getWindowVisibleDisplayFrame(frame);
+        int statusBarHeight = frame.top;
+        int width = activity.getWindowManager().getDefaultDisplay().getWidth();
+        int height = activity.getWindowManager().getDefaultDisplay().getHeight();
+        Bitmap b = Bitmap.createBitmap(b1, 0, statusBarHeight, width, height  - statusBarHeight);
+        view.destroyDrawingCache();
+        return b;
+    }
+
+    private Bitmap fastblur(Bitmap sentBitmap) {
+
+        Bitmap old = sentBitmap;
+        int width = Math.round(sentBitmap.getWidth() * 0.2f);
+        int height = Math.round(sentBitmap.getHeight() * 0.2f);
+        sentBitmap = Bitmap.createScaledBitmap(sentBitmap, width, height, false);
+        Bitmap bitmap = sentBitmap.copy(sentBitmap.getConfig(), true);
+        if (6 < 1) { return (null); }
+        int w = bitmap.getWidth();
+        int h = bitmap.getHeight();
+        int[] pix = new int[w * h];
+        bitmap.getPixels(pix, 0, w, 0, 0, w, h);
+        int wm = w - 1;
+        int hm = h - 1;
+        int wh = w * h;
+        int div = 6 + 6 + 1;
+        int r[] = new int[wh];
+        int g[] = new int[wh];
+        int b[] = new int[wh];
+        int rsum, gsum, bsum, x, y, i, p, yp, yi, yw;
+        int vmin[] = new int[Math.max(w, h)];
+        int divsum = (div + 1) >> 1;
+        divsum *= divsum;
+        int dv[] = new int[256 * divsum];
+        for (i = 0; i < 256 * divsum; i++) { dv[i] = (i / divsum); }
+        yw = yi = 0;
+        int[][] stack = new int[div][3];
+        int stackpointer;
+        int stackstart;
+        int[] sir;
+        int rbs;
+        int r1 = 6 + 1;
+        int routsum, goutsum, boutsum;
+        int rinsum, ginsum, binsum;
+        for (y = 0; y < h; y++) {
+            rinsum = ginsum = binsum = routsum = goutsum = boutsum = rsum = gsum = bsum = 0;
+            for (i = -6; i <= 6; i++) {
+                p = pix[yi + Math.min(wm, Math.max(i, 0))];
+                sir = stack[i + 6];
+                sir[0] = (p & 0xff0000) >> 16;
+                sir[1] = (p & 0x00ff00) >> 8;
+                sir[2] = (p & 0x0000ff);
+                rbs = r1 - Math.abs(i);
+                rsum += sir[0] * rbs;
+                gsum += sir[1] * rbs;
+                bsum += sir[2] * rbs;
+                if (i > 0) {
+                    rinsum += sir[0];
+                    ginsum += sir[1];
+                    binsum += sir[2];
+                } else {
+                    routsum += sir[0];
+                    goutsum += sir[1];
+                    boutsum += sir[2];
+                }
+            }
+            stackpointer = 6;
+            for (x = 0; x < w; x++) {
+                r[yi] = dv[rsum];
+                g[yi] = dv[gsum];
+                b[yi] = dv[bsum];
+                rsum -= routsum;
+                gsum -= goutsum;
+                bsum -= boutsum;
+                stackstart = stackpointer - 6 + div;
+                sir = stack[stackstart % div];
+                routsum -= sir[0];
+                goutsum -= sir[1];
+                boutsum -= sir[2];
+                if (y == 0) { vmin[x] = Math.min(x + 6 + 1, wm); }
+                p = pix[yw + vmin[x]];
+                sir[0] = (p & 0xff0000) >> 16;
+                sir[1] = (p & 0x00ff00) >> 8;
+                sir[2] = (p & 0x0000ff);
+                rinsum += sir[0];
+                ginsum += sir[1];
+                binsum += sir[2];
+                rsum += rinsum;
+                gsum += ginsum;
+                bsum += binsum;
+                stackpointer = (stackpointer + 1) % div;
+                sir = stack[(stackpointer) % div];
+                routsum += sir[0];
+                goutsum += sir[1];
+                boutsum += sir[2];
+                rinsum -= sir[0];
+                ginsum -= sir[1];
+                binsum -= sir[2];
+                yi++;
+            }
+            yw += w;
+        }
+        for (x = 0; x < w; x++) {
+            rinsum = ginsum = binsum = routsum = goutsum = boutsum = rsum = gsum = bsum = 0;
+            yp = -6 * w;
+            for (i = -6; i <= 6; i++) {
+                yi = Math.max(0, yp) + x;
+                sir = stack[i + 6];
+                sir[0] = r[yi];
+                sir[1] = g[yi];
+                sir[2] = b[yi];
+                rbs = r1 - Math.abs(i);
+                rsum += r[yi] * rbs;
+                gsum += g[yi] * rbs;
+                bsum += b[yi] * rbs;
+                if (i > 0) {
+                    rinsum += sir[0];
+                    ginsum += sir[1];
+                    binsum += sir[2];
+                } else {
+                    routsum += sir[0];
+                    goutsum += sir[1];
+                    boutsum += sir[2];
+                }
+                if (i < hm) {
+                    yp += w;
+                }
+            }
+            yi = x;
+            stackpointer = 6;
+            for (y = 0; y < h; y++) {
+                // Preserve alpha channel: ( 0xff000000 & pix[yi] )
+                pix[yi] = ( 0xff000000 & pix[yi] ) | ( dv[rsum] << 16 ) | ( dv[gsum] << 8 ) | dv[bsum];
+                rsum -= routsum;
+                gsum -= goutsum;
+                bsum -= boutsum;
+                stackstart = stackpointer - 6 + div;
+                sir = stack[stackstart % div];
+                routsum -= sir[0];
+                goutsum -= sir[1];
+                boutsum -= sir[2];
+                if (x == 0) { vmin[y] = Math.min(y + r1, hm) * w; }
+                p = x + vmin[y];
+                sir[0] = r[p];
+                sir[1] = g[p];
+                sir[2] = b[p];
+                rinsum += sir[0];
+                ginsum += sir[1];
+                binsum += sir[2];
+                rsum += rinsum;
+                gsum += ginsum;
+                bsum += binsum;
+                stackpointer = (stackpointer + 1) % div;
+                sir = stack[stackpointer];
+                routsum += sir[0];
+                goutsum += sir[1];
+                boutsum += sir[2];
+                rinsum -= sir[0];
+                ginsum -= sir[1];
+                binsum -= sir[2];
+                yi += w;
+            }
+        }
+        bitmap.setPixels(pix, 0, w, 0, 0, w, h);
+        bitmap = Bitmap.createScaledBitmap(bitmap, old.getWidth(), old.getHeight(), false);
+        return (bitmap);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(sfondoSfocato!=null){
+            sfondoSfocato.setVisibility(View.GONE);
+        }
+        super.onBackPressed();
     }
 
 }
---
+++ app/src/main/java/com/example/armando/marketbook/RisultatiAdapter.java
@@ -0,0 +1,171 @@
+package com.example.armando.marketbook;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class RisultatiAdapter extends RecyclerView.Adapter<RisultatiAdapter.MyView> {
+
+    private List<Object> risultati;
+    private Context context;
+    private OnItemClickListener mItemClickListener;
+    private File directory;
+
+    public class MyView extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private TextView Titolo;
+        private TextView Prezzo;
+        private ImageView Immagine;
+        private ProgressBar progressBar;
+
+        MyView(View view) {
+            super(view);
+            view.setOnClickListener(this);
+            Titolo = view.findViewById(R.id.Nome_Titolo);
+            Immagine = view.findViewById(R.id.Immagine);
+            Prezzo = view.findViewById(R.id.prezzo);
+            progressBar = view.findViewById(R.id.progressBar);
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (mItemClickListener != null) {
+                mItemClickListener.onItemClick(view, risultati.get(getLayoutPosition()));
+            }
+        }
+    }
+
+    RisultatiAdapter(Context context, List<Object> risultati) {
+        this.risultati = risultati;
+        this.context = context;
+        directory = context.getDir("immagini", Context.MODE_PRIVATE);
+        if (!directory.exists())
+        {
+            directory.mkdirs();
+        }
+    }
+
+    RisultatiAdapter(Context context){
+        directory = context.getDir("immagini", Context.MODE_PRIVATE);
+        if (!directory.exists())
+        {
+            directory.mkdirs();
+        }
+    }
+
+    @NonNull
+    @Override
+    public MyView onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.riga, parent, false);
+        return new MyView(itemView);
+    }
+
+    //Specifica il contenuto di ciascun elemento della RecyclerView
+    @Override
+    public void onBindViewHolder(@NonNull final MyView holder, final int position) {
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageRef;
+        final DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+        try {
+            Book oggetto = (Book) risultati.get(position);
+            holder.Titolo.setText(oggetto.getTitolo());
+            holder.Prezzo.setText(String.valueOf(oggetto.getPrezzo()) + "\u20ac");
+            storageRef = storage.getReferenceFromUrl("gs://books-c7269.appspot.com").child(oggetto.getURLCopertina());
+            final File file = new File(directory.getAbsolutePath(), (String) oggetto.getTitolo() + ".png");
+            if (!file.exists()) {
+                storageRef.getFile(file).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                        Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
+                        bitmap = Bitmap.createScaledBitmap(bitmap, Math.round(48 * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT)), Math.round(64 * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT)), true);
+                        holder.Immagine.setImageBitmap(bitmap);
+                        holder.progressBar.setVisibility(View.GONE);
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        exception.printStackTrace();
+                    }
+                });
+            } else {
+                Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
+                bitmap = Bitmap.createScaledBitmap(bitmap, Math.round(48 * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT)), Math.round(64 * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT)), true);
+                holder.Immagine.setImageBitmap(bitmap);
+                holder.progressBar.setVisibility(View.GONE);
+            }
+        } catch (Exception e) {
+
+        }
+        try {
+            Autore oggetto = (Autore) risultati.get(position);
+            holder.Titolo.setText(oggetto.getNome());
+            storageRef = storage.getReferenceFromUrl("gs://books-c7269.appspot.com").child(oggetto.getUrlImmagine());
+            final File file = new File(directory.getAbsolutePath(), (String) oggetto.getNome() + ".png");
+            if (!file.exists()) {
+                storageRef.getFile(file).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                        Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
+                        bitmap = Bitmap.createScaledBitmap(bitmap, Math.round(64 * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT)), Math.round(64 * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT)), true);
+                        holder.Immagine.setImageBitmap(bitmap);
+                        holder.progressBar.setVisibility(View.GONE);
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+                            exception.printStackTrace();
+                        }
+                });
+            } else {
+                Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
+                bitmap = Bitmap.createScaledBitmap(bitmap, Math.round(64 * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT)), Math.round(64 * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT)), true);
+                holder.Immagine.setImageBitmap(bitmap);
+                holder.progressBar.setVisibility(View.GONE);
+            }
+        } catch (Exception e) {
+
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return (risultati.size());
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(View view, Object object);
+    }
+
+    public void SetOnItemClickListener(final OnItemClickListener mItemClickListener) {
+        this.mItemClickListener = mItemClickListener;
+    }
+
+    public void destroy(){
+        File file = new File(directory.getAbsolutePath());
+        String[] files;
+        files = file.list();
+        for (int i=0; i<files.length; i++) {
+            File myFile = new File(file, files[i]);
+            myFile.delete();
+        }
+    }
+
+}
